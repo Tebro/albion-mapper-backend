b21c425aa0e2e5ddf1c914456cc0b9e4c2be90a7
Richard Weber <riche.weber@gmail.com>
2020-08-30 10:10:35 +0300

    Add data conversion script

---
+++ data-conveter/main.py
@@ -0,0 +1,30 @@
+import sys
+import json
+
+
+def handle_json(data):
+  cluster = data["world"]["clusters"]["cluster"]
+  selected_fields = [{"name": m["@displayname"], "type": m["@type"]} for m in cluster]
+
+  filtered_by_type = [o for o in selected_fields if (o["type"].startswith("OPENPVP") or o["type"].startswith("TUNNEL"))]
+
+  print(json.dumps(filtered_by_type))
+
+  #distinct_types = set([o["type"] for o in filtered_by_type])
+  #print(distinct_types)
+
+  #name_to_find = "Xerites-Oxoulum"
+  #name_to_find = "Whitebank Cross"
+  #name_to_find = "Qiient-Qinsum"
+  #name_to_find = "Wyre Forest"
+  #print([o for o in selected_fields if o["name"] == name_to_find])
+
+
+
+file_path = sys.argv[1]
+
+with open(file_path) as file:
+  raw = file.read()
+  parsed = json.loads(raw)
+
+  handle_json(parsed)
